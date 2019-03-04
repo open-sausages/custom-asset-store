ad1ade6566fa95ebaaac54be0f0026dcefead874
Maxime Rainville <maxime@silverstripe.com>
2019-03-04 18:58:28 +1300

    Get the basic logic working with old hash URL.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Silverstripe Custom Asset Store
+
+This is an experimental module that demonstrate how you can customise your asset store to match legacy style URLs or old publish file hashes.
+
+## To install
+
+Add the following entry to your composer.json.
+```json
+{
+    ...
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "git@github.com:open-sausages/custom-asset-store.git"
+        }
+    ]
+}
+```
+
+Install the package with:
+```bash
+composer require silverstripe/custom-asset-store
+```
+
+## Why is this needed
+
+By default, your project SilverStripe 4 will publish files under a URL containing an hash (e.g.: `/assets/Uploads/80864e1046/sam.jpg`). The hash part of the URL is based on the content of the file. If you upload a new version of the file, the hash will change which will change the URL. Direct links to old version of the files will break when this happens.
+
+Also, if you upgraded your site from SilverStripe 3, old links to you file won't work anymore because SS3 didn't have the hash at all.
+
+## What does this actually do
+
+The custom asset store included in this module will try to match previously published file URL and legacy file URL to a valid published file. If it does, it will serve the content of the last published version of that file.
+
+This will prevent broken links. Note that authenticated users bypass this logic to avoid blocking access to draft files. 
+
+## Isn't this a bit hackish?
+
+Yes it is. We have a [better solution in the works](https://github.com/silverstripe/silverstripe-versioned/issues/177).
--- src/CustomAssetStore.php
+++ src/CustomAssetStore.php
@@ -3,20 +3,82 @@ namespace SilverStripe\CustomAssetsStore;
 
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Flysystem\FlysystemAssetStore;
+use SilverStripe\Security\Security;
+use SilverStripe\Versioned\Versioned;
 
+/**
+ * This class extends `FlysystemAssetStore` so you can match old published file hash
+ * (/assets/Uploads/80864e1046/sam.jpg) or legacy style file names (/assets/Uploads/sam.jpg).
+ *
+ * The latest published version of the file will be return to the browser.
+ */
 class CustomAssetStore extends FlysystemAssetStore {
 
     public function getResponseFor($asset)
     {
-        /**
-         * @var File
-         */
-        $file = File::get()->filter(['FileFilename' => $asset])->first();
-        if ($file) {
-            $asset = $file->File->getSourceURL();
-            $asset = preg_replace('/^\/?assets\//i', '', $asset);
+        /** @var boolean|File $file */
+        $file = false;
+        $parsedFileID = $this->parseFileID($asset);
+
+        // We skipped this part for authenticated user otherwise, you might not be able to access a protected file
+        if (!Security::getCurrentUser() && $parsedFileID && isset($parsedFileID['Hash'])) {
+            
+            $file = File::get()->filter(['FileFilename' => $parsedFileID['Filename']])->first();
+
+            // If we find a file with the matched Filename, let's look to see if we find a version that matches our Hash
+            if ($file) {
+                $archivedFile = $file->allVersions(
+                    [
+                        // There's SQL Injection risk here because parseFileID won't match invalid hashes
+                        ['FileHash like ?' => $parsedFileID['Hash'] . '%'],
+                        'WasPublished' => true
+                    ],
+                    ['ID' => 'Desc'], 1
+                )->first();
+                // If we found a version that matches our hash, let's return the url to the latest file.
+                if ($archivedFile) {
+                    $asset = $file->File->getSourceURL();
+                    $asset = preg_replace('/^\/?assets\//i', '', $asset);
+                }
+            }
+        } else {
+            // Let's try to match the plain file name
+            $file = File::get()->filter(['FileFilename' => $asset])->first();
+            if ($file) {
+                $asset = $file->File->getSourceURL();
+                $asset = preg_replace('/^\/?assets\//i', '', $asset);
+            }
         }
 
         return parent::getResponseFor($asset);
+    }
+
+    /**
+     * Get Filename and Variant from fileid
+     *
+     * @note This method is identical to the parent implementation, except we return the Hash as well
+     * @param string $fileID
+     * @return array
+     */
+    protected function parseFileID($fileID)
+    {
+        if ($this->useLegacyFilenames()) {
+            $pattern = '#^(?<folder>([^/]+/)*)(?<basename>((?<!__)[^/.])+)(__(?<variant>[^.]+))?(?<extension>(\..+)*)$#';
+        } else {
+            $pattern = '#^(?<folder>([^/]+/)*)(?<hash>[a-zA-Z0-9]{10})/(?<basename>((?<!__)[^/.])+)(__(?<variant>[^.]+))?(?<extension>(\..+)*)$#';
+        }
+
+        // not a valid file (or not a part of the filesystem)
+        if (!preg_match($pattern, $fileID, $matches)) {
+            return null;
+        }
+
+        $filename = $matches['folder'] . $matches['basename'] . $matches['extension'];
+        $variant = isset($matches['variant']) ? $matches['variant'] : null;
+        return [
+            'Filename' => $filename,
+            'Variant' => $variant,
+            'Hash' =>  isset($matches['hash']) ? $matches['hash'] : '' 
+        ];
     }
 }
